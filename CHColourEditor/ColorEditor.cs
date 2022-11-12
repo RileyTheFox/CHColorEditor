bd66215685cf7ce59d14488d720a955ea8e614de
RileyTheFox <jacobchambers@hotmail.co.uk>
2022-11-12 22:44:48 +0000

    Updated version string to 1.4

--- CHColourEditor/ColorEditor.cs
+++ CHColourEditor/ColorEditor.cs
@@ -20,9 +20,9 @@ namespace CHColourEditor
     public sealed partial class ColorEditor : Form
     {
 #if DEBUG
-        private const string CURRENT_VERSION_STRING = "1.3 DEVELOPMENT BUILD";
+        private const string CURRENT_VERSION_STRING = "1.4 DEVELOPMENT BUILD";
 #else
-        private const string CURRENT_VERSION_STRING = "1.3";
+        private const string CURRENT_VERSION_STRING = "1.4";
 #endif
 
         private const string TITLE_STRING = "CH Color Editor v" + CURRENT_VERSION_STRING;
